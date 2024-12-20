ac7efb7d03ef4cda829a58e2dc0a35623132855e
Abhimanyu <abhimanyu@costacloud.com>
2024-12-20 12:15:03 +0530

    Home Protected

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,17 @@ import Login from "./Login";
 import Home from "./Home";
 import { Toaster } from "react-hot-toast";
 import Protected from "./Protected";
-import { Menu } from "@mui/material";
 
 const App = () => {
   return (
-    <Menu />
-    // <BrowserRouter>
-    //   <Toaster />
-    //   <Routes>
-    //     {/* Define the home route */}
-    //     <Route path="/login" element={<Login />} />
-    //     <Route path="/home" element={<Protected Component={Home} />} />
-    //   </Routes>
-    // </BrowserRouter>
+    <BrowserRouter>
+      <Toaster />
+      <Routes>
+        {/* Define the home route */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Protected Component={Home} />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
